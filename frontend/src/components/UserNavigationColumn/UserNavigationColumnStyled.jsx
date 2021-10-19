df6115f19d1b2730f93048b7039efce463865146
David Furrer <david.furrer1@gmail.com>
2021-10-19 18:22:33 +0200

    finished

--- frontend/src/components/UserNavigationColumn/UserNavigationColumnStyled.jsx
+++ frontend/src/components/UserNavigationColumn/UserNavigationColumnStyled.jsx
@@ -1,17 +1,18 @@
 import styled from "styled-components";
 
 export const UserNavigationWrapper = styled.div`
-  height: 46.75px;
   width: 232px;
   background: white;
 `;
 
 export const UserNavigationRow = styled.div`
   width: 100%;
+  height: 46.75px;
   display: flex;
   background: white;
   /* justify-content: center; */
   align-items: center;
+  border-bottom: 1px gray solid;
 `;
 
 export const UserNavigationRowText = styled.div`
@@ -23,19 +24,31 @@ export const UserNavigationRowText = styled.div`
 export const CommentIcon = styled.img`
   margin-right: 15px;
   cursor: pointer;
+  width: 28px;
+  height: 28px;
+  margin-left: 3%;
 `;
 
 export const EditIcon = styled.img`
   margin-right: 15px;
   cursor: pointer;
+  width: 28px;
+  height: 28px;
+  margin-left: 3%;
 `;
 
 export const RestaurantIcon = styled.img`
   margin-right: 15px;
   cursor: pointer;
+  width: 28px;
+  height: 28px;
+  margin-left: 3%;
 `;
 
 export const StarIcon = styled.img`
   margin-right: 15px;
   cursor: pointer;
+  width: 28px;
+  height: 28px;
+  margin-left: 3%;
 `;
