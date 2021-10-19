003a9ec52e08b520606916c52b2dd1178a3658e0
David Furrer <david.furrer1@gmail.com>
2021-10-19 11:25:05 +0200

    finished about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,26 @@
-import {BrowserRouter, Route} from "react-router-dom";
-import Home from './Pages/Home';
-import Search from './Pages/Search';
-import Restaurant from './Pages/Restaurant';
-import Review from './Pages/Review';
-import NewRestaurant from './Pages/NewRestaurant';
-import UserProfile from './Pages/UserProfile';
-import Registration from './Pages/Registration';
-import Login from './Pages/Login';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Search from "./Pages/Search";
+import Restaurant from "./Pages/Restaurant";
+import Review from "./Pages/Review";
+import NewRestaurant from "./Pages/NewRestaurant";
+import UserProfile from "./Pages/UserProfile";
+import Registration from "./Pages/Registration";
+import Login from "./Pages/Login";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/search" component={Search}/>
-            <Route exact path="/restaurant" component={Restaurant}/>
-            <Route exact path="/review" component={Review}/>
-            <Route exact path="/newrestaurant" component={NewRestaurant}/>
-            <Route exact path="/profile/:username" component={UserProfile}/>
-            <Route exact path="/registration" component={Registration}/>
-            <Route exact path="/login" component={Login}/>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/restaurant" component={Restaurant} />
+      <Route exact path="/review" component={Review} />
+      <Route exact path="/newrestaurant" component={NewRestaurant} />
+      <Route exact path="/profile/:username" component={UserProfile} />
+      <Route exact path="/registration" component={Registration} />
+      <Route exact path="/login" component={Login} />
+    </BrowserRouter>
+  );
 };
-
 
 export default App;
---
+++ frontend/src/components/UserAbout/UserAboutStyled.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const UserAboutWrapper = styled.div`
+  width: 500px;
+  height: 500px;
+`;
+
+export const UserAboutTitle = styled.div`
+  color: #303030;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export const UserAboutItemTitle = styled.div`
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: 3%;
+`;
+
+export const UserAboutContent = styled.div`
+  font-weight: 300;
+  font-size: 20px;
+  margin-top: 1%;
+`;
