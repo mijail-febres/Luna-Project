ffc318735cc4674cc8bf10ec7c84d46030113cdc
hgeorgiev101 <hgeorgiev101@gmail.com>
2021-10-21 17:21:56 +0200

    initial commit

--- frontend/src/Pages/UserProfile/index.jsx
+++ frontend/src/Pages/UserProfile/index.jsx
@@ -2,13 +2,17 @@ import UserAbout from "../../components/UserAbout/UserAbout";
 import MainHeader from "../../components/MainHeader/MainHeader";
 import MainFooter from "../../components/MainFooter/MainFooter";
 import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setToken, getUserInfo } from "../../store/actions";
 
 const UserProfilePageWrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  display: flex;
+  /* display: flex; */
   background: #f2f2f2;
-  flex-wrap: wrap;
+  /* flex-wrap: wrap; */
 `;
 
 const UserAboutWrapper = styled.div`
@@ -20,6 +24,44 @@ const UserAboutWrapper = styled.div`
 `;
 
 const UserProfile = (props) => {
+  const [reviews, setReviews] = useState("");
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const tokenRedux = useSelector((state) => state.user.token);
+  const userInfo = useSelector((state) => state.user.userInfo);
+  console.log("reviews", reviews);
+
+  const getTokenSetUser = () => {
+    const tokenInLocalStorage = localStorage.getItem("luna-auth-token");
+    if (tokenInLocalStorage) {
+      dispatch(setToken(tokenInLocalStorage));
+      dispatch(() => getUserInfo(dispatch, tokenInLocalStorage));
+    } else {
+      history.push("/");
+    }
+  };
+
+  const getReviews = async () => {
+    const tokenInLocalStorage = localStorage.getItem("luna-auth-token");
+    if (userInfo) {
+      const url = `https://luna-dhmp.propulsion-learn.ch/backend/api/reviews/user/${userInfo.id}/`;
+      const headers = new Headers({
+        Authorization: `Bearer ${tokenInLocalStorage}`,
+      });
+      const config = {
+        headers,
+      };
+      fetch(url, config)
+        .then((res) => res.json())
+        .then((data) => setReviews(data));
+    }
+  };
+
+  useEffect(() => {
+    getTokenSetUser();
+    getReviews();
+  }, []);
+
   return (
     <UserProfilePageWrapper>
       <MainHeader />
