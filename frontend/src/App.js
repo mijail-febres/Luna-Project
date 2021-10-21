71bcd3b476282446939332897fe592ca162f7b3a
mijaplex <mijail.febres@gmail.com>
2021-10-21 16:19:08 +0200

    App fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,35 +7,18 @@ import NewRestaurant from "./Pages/NewRestaurant";
 import UserProfile from "./Pages/UserProfile";
 import Registration from "./Pages/Registration";
 import Login from "./Pages/Login";
-import UsersBody from "./components/UsersBody/UsersBody";
-import ReviewForm from "./components/ReviewForm/ReviewForm";
-import NavigationSubheader from "./components/NavigationSubheader/NavigationSubheader";
 
 const App = () => {
   return (
     <BrowserRouter>            
-      {/* <NavigationSubheader></NavigationSubheader> */}
-      {/* <ReviewForm id_restaurant='2'></ReviewForm> */}
-      {/* <UsersBody></UsersBody> */}
-      {/* <ReviewPreview 
-        user_name='Laurent H.' 
-        user_nReviews='6' 
-        restaurant_name='Colinz Bar' 
-        contentReview="Ugh. Don't waste your time. Pizza dough good, thin crust but ingredients so so. Side of mixed vegetables very oily and mainly bell"
-        likes = "5"
-        comments = "10"
-        >
-
-      </ReviewPreview> */}
-    <Route exact path="/restaurant" component={Restaurant} />
-    {/* <Route exact path="/" component={Home} />
+    <Route exact path="/" component={Home} />
     <Route exact path="/search" component={Search} />
     <Route exact path="/restaurant" component={Restaurant} />
     <Route exact path="/review" component={Review} />
     <Route exact path="/newrestaurant" component={NewRestaurant} />
     <Route exact path="/profile/:username" component={UserProfile} />
     <Route exact path="/registration" component={Registration} />
-    <Route exact path="/login" component={Login} /> */}
+    <Route exact path="/login" component={Login} />
     </BrowserRouter>
   );
 };
