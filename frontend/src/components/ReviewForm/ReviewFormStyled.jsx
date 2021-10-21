cc2b5929f162457e95135ee849ad4a6f6b4861f6
David Furrer <david.furrer1@gmail.com>
2021-10-21 13:51:39 +0200

    finishing createReview page (css not perfect yet)

--- frontend/src/components/ReviewForm/ReviewFormStyled.jsx
+++ frontend/src/components/ReviewForm/ReviewFormStyled.jsx
@@ -1,51 +1,52 @@
 import styled from "styled-components";
 
 const ReviewFormContainer = styled.div`
-    width: 100%;
-    height: 665px;
-    box-sizing: content-box;
+  z-index: 10;
+  width: 100%;
+  height: 465px;
+  box-sizing: content-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f8f8f8;
+  #main {
+    width: 832px;
+    height: 431px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background-color: #F8F8F8;
-    #main {
-        width: 832px;
-        height: 431px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        #header {
-            width: 100%;
-            height: 96px;
-            display: flex;
-            justify-content: flex-start;
-            align-items: center;
-            #selectRating {
-                padding: 10px;
-            }
-        }
-        #body {
-            width: 100%;
-            height: 265px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            #review{
-                width: 100%;
-                height: 100%;
-                border: solid 1px #E3E3E3;
-            }
-        }
-        #footer {
-            width: 100%;
-            height: 75px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
+    #header {
+      width: 100%;
+      height: 96px;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      #selectRating {
+        padding: 10px;
+      }
     }
+    #body {
+      width: 100%;
+      height: 265px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      #review {
+        width: 100%;
+        height: 100%;
+        border: solid 1px #e3e3e3;
+      }
+    }
+    #footer {
+      width: 100%;
+      height: 75px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
 `;
 
 export default ReviewFormContainer;
