fe43ddad3f2d18447ad9438ae49577c2d39002e8
hgeorgiev101 <hgeorgiev101@gmail.com>
2021-10-22 09:41:08 +0200

    reviews endpoint finished, search endpoint bugfixed, total comments of user added

--- backend/restaurant_review/urls.py
+++ backend/restaurant_review/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from restaurant_review.views import CreateRestaurantReviewView, ListRestaurantReviewByRestaurantIDView, \
     ListRestaurantReviewsByUserIDView, RetrieveUpdateDeleteRestaurantReviewView, ToggleLikeRestaurantReviewView, \
-    ListLikedReviewsByCurrentUserView, ListCommentedReviewsByCurrentUserView
+    ListLikedReviewsByCurrentUserView, ListCommentedReviewsByCurrentUserView, ListAllReviewsView
 
 urlpatterns = [
     path('new/<int:restaurant_id>/', CreateRestaurantReviewView.as_view()),
@@ -12,4 +12,5 @@ urlpatterns = [
     path('like/<int:review_id>/', ToggleLikeRestaurantReviewView.as_view()),
     path('likes/', ListLikedReviewsByCurrentUserView.as_view()),
     path('comments/', ListCommentedReviewsByCurrentUserView.as_view()),
+    path('all/', ListAllReviewsView.as_view()),
 ]
